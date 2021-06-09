abbfb8e37a4a28c9d3e5f66aa5b0b1eb08b2db1e
Nicole Christian <nicole.jc.0101@gmail.com>
2021-06-09 19:23:19 -0400

    Initial check ins

---
+++ MarsRoversNicole/Mars.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoversNicole
+{
+    class Mars
+    {
+        static int movementGrid_x;
+        static int movementGrid_y;
+        public void SetMovementGrid(int column, int row)
+        {
+            movementGrid_x = column;
+            movementGrid_y = row;
+        }
+
+        /// <summary>
+        /// Method used to move the rover. Takes in the starting x position, the starting y position, the starting rotation, and the string of instructions for movement.
+        /// SetMovementGrid() is called before this method to establish the grid dimensions.
+        /// </summary>
+        /// <returns>Tuple<int, int, Rotation, bool>: roverlocation, roverrotation, successful movement (if the rover was successfully able to complete its movement in full)</returns>
+        public Tuple<int, int, Rotation, bool> MoveRover(int startColumn, int startRow, Rotation startRotation, string movementInstructions) {
+            MarsRover marsRoverCreated = new MarsRover(startColumn, startRow, startRotation);
+            Tuple<int, int, Rotation> roverInformation;
+            if (!marsRoverCreated.CalculateMovement(movementInstructions))
+            {
+                roverInformation = marsRoverCreated.getRoverCurrentPositionRotation();
+                return new Tuple<int, int, Rotation, bool>(roverInformation.Item1, roverInformation.Item2, roverInformation.Item3, false);
+            }
+            roverInformation = marsRoverCreated.getRoverCurrentPositionRotation();
+            return new Tuple<int, int, Rotation, bool>(roverInformation.Item1, roverInformation.Item2, roverInformation.Item3, true);
+        }
+
+        class MarsRover
+        {
+            int columnCurrentPosition;
+            int rowCurrentPosition;
+            Rotation currentRotation;
+            public MarsRover(int columnPostion, int rowPositon, Rotation rotation)
+            {
+                columnCurrentPosition = columnPostion;
+                rowCurrentPosition = rowPositon;
+                currentRotation = rotation;
+            }
+
+            public Tuple<int, int, Rotation> getRoverCurrentPositionRotation()
+            {
+                return new Tuple<int, int, Rotation>(columnCurrentPosition, rowCurrentPosition, currentRotation);
+            }
+            Rotation getRoverCurrentRotation()
+            {
+                return currentRotation;
+            }
+            private bool setRoverCurrentPosition(int column, int row)
+            {
+                if (!validRoverPosition(column, row))
+                {
+                    return false;
+                }
+                columnCurrentPosition = column;
+                rowCurrentPosition = row;
+                return true;
+            }
+            private void setRoverCurrentRotation(int rotation)
+            {
+                currentRotation = (Rotation)rotation;
+            }
+
+            /// <summary>
+            /// Compares the rover position to the grid. If the desired position would cause the rover to move off of the grid, it returns false.
+            /// </summary>
+            public bool validRoverPosition(int column, int row)
+            {
+                if (column > movementGrid_x || column < 0 || row > movementGrid_y || row < 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            /// <summary>
+            /// Moves the rover based on the inputted string parameter.
+            /// </summary>
+            /// <returns>bool: was this movement successful in full</returns>
+            public bool CalculateMovement(string movementInstructionString)
+            {
+                Tuple<int, int, Rotation> roverPosition = getRoverCurrentPositionRotation();
+                if (!validRoverPosition(roverPosition.Item1, roverPosition.Item2))
+                {
+                    return false;
+                }
+                foreach (char instruction in movementInstructionString.Trim().ToLower())
+                {
+
+                    if (instruction == 'r')
+                    {
+                        if (getRoverCurrentRotation() == Rotation.W)
+                        {
+                            setRoverCurrentRotation(0);
+                        }
+                        else
+                        {
+                            setRoverCurrentRotation((int)getRoverCurrentRotation() + 1);
+                        }
+                    }
+                    else if (instruction == 'l')
+                    {
+                        if (getRoverCurrentRotation() == Rotation.N)
+                        {
+                            setRoverCurrentRotation(3);
+                        }
+                        else
+                        {
+                            setRoverCurrentRotation((int)getRoverCurrentRotation() - 1);
+                        }
+                    }
+                    else if (instruction == 'm')
+                    {
+                        bool movementSuccessful = false;
+                        switch (roverPosition.Item3)
+                        {
+                            case Rotation.N:
+                                movementSuccessful = setRoverCurrentPosition(roverPosition.Item1, roverPosition.Item2 + 1);
+                                break;
+                            case Rotation.E:
+                                movementSuccessful = setRoverCurrentPosition(roverPosition.Item1 + 1, roverPosition.Item2);
+                                break;
+                            case Rotation.S:
+                                movementSuccessful = setRoverCurrentPosition(roverPosition.Item1, roverPosition.Item2 - 1);
+                                break;
+                            case Rotation.W:
+                                movementSuccessful = setRoverCurrentPosition(roverPosition.Item1 - 1, roverPosition.Item2);
+                                break;
+                        }
+                        if (!movementSuccessful)
+                        {
+                            return false;
+                        }
+                    }
+                    roverPosition = getRoverCurrentPositionRotation();
+                   
+                }
+                return true;
+            }
+
+
+
+
+
+
+        }
+
+       
+    }
+
+}
+
+
---
+++ MarsRoversNicole/NASAProgram.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRoversNicole
+{
+    public class NASAProgram
+    {
+        static void Main(string[] args)
+        {
+            StartCommunications();
+        }
+
+        static List<string> roverLocations = new List<string>();
+        static Tuple<int, int, Rotation, bool> newRoverLocation;
+        static int gridX;
+        static int gridY;
+        static List<int> roverX = new List<int>();
+        static List<int> roverY = new List<int>();
+        static List<Rotation> roverRotation = new List<Rotation>();
+        static List<string> movementInstructions = new List<string>();
+
+
+        /// <summary>
+        /// Handles resetting all variables needed for a new iteration.
+        /// </summary>
+        static void ResetVariables()
+        {
+            roverLocations = new List<string>();
+            newRoverLocation = null;
+            gridX = 0;
+            gridY = 0;
+            roverX = new List<int>();
+            roverY = new List<int>();
+            roverRotation = new List<Rotation>();
+            movementInstructions = new List<string>();
+        }
+
+        static void StartCommunications()
+        {
+            Mars marsConnection = new Mars();
+            try
+            {
+                startOver:
+                ResetVariables();
+                if (getGrid())
+                {
+                    if (newRover())
+                    {
+                        marsConnection.SetMovementGrid(gridX, gridY);
+                        //All data inputs have been collect, time to calculate the movement of the rover per rover.
+                        for (int roverCount = 0; roverCount < roverX.Count; roverCount++)
+                        {
+                            newRoverLocation = marsConnection.MoveRover(roverX[roverCount], roverY[roverCount], roverRotation[roverCount], movementInstructions[roverCount]);
+                            //Item4 of the returned Tuple determines if the rover was successfully able to complete its movement. In other words, stopping it before it moves off of the grid.
+                            if (!newRoverLocation.Item4)
+                            {
+                                Console.WriteLine("Rover was unable to move any further with the instructions given.");
+                            }
+                            roverLocations.Add(newRoverLocation.Item1 + " " + newRoverLocation.Item2 + " " + newRoverLocation.Item3);
+                        }
+                    }
+                }
+
+                foreach (var roverMovementLocations in roverLocations)
+                {
+                    Console.WriteLine(roverMovementLocations);
+                }
+
+                Console.WriteLine("--------");
+                Console.WriteLine("Press Enter to begin anew. Press Escape to exit.");
+                var nextInput = Console.ReadKey();
+                if (nextInput.Key != ConsoleKey.Escape)
+                {
+                    goto startOver;
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("An exception was thrown: " + err.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves user input for grid dimensions.
+        /// </summary>
+        /// <returns>bool: did user prompt to exit</returns>
+        static bool getGrid()
+        {
+            try
+            {
+                bool continueCheck = false;
+                while (!continueCheck)
+                {
+                    Console.Write("Coordinate grid dimensions (x y): ");
+                    string gridDimensions = Console.ReadLine().Trim();
+                    if (string.IsNullOrWhiteSpace(gridDimensions))
+                    {
+                        Console.WriteLine("END");
+                        return false;
+                    }
+                    string[] splited = gridDimensions.Split(" ");
+                    if (splited.Length != 2 || gridDimensions.Any(char.IsLetter) || (splited.All(e => e.Equals("0"))))
+                    {
+                        Console.WriteLine("Invalid input. Please use format \"x y\"");
+                        continueCheck = false;
+                    }
+                    else
+                    {
+                        gridX = Int32.Parse(splited[0]);
+                        gridY = Int32.Parse(splited[1]);
+                        continueCheck = true;
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("An exception was thrown at grid dimensions input: " + err.StackTrace);
+                throw;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieves user input for a new rover. 1. Rover starting position and 2. Movement instructions for that rover.
+        /// </summary>
+        /// <returns>bool: did user prompt to exit</returns>
+        static bool newRover(string newString = "")
+        {
+            try
+            {
+                bool continueCheck = false;
+                while (!continueCheck)
+                {
+                    string roverStartPosition = newString;
+                    if (string.IsNullOrEmpty(newString))
+                    {
+                        Console.Write("Rover starting position (x y r): ");
+                        roverStartPosition = Console.ReadLine().Trim().ToLower();
+                    }
+
+                    if (string.IsNullOrWhiteSpace(roverStartPosition))
+                    {
+                        Console.WriteLine("END");
+                        return false;
+                    }
+
+                    string[] splited = roverStartPosition.Split(" ");
+                    if (splited.Length != 3 || !(roverStartPosition.Contains("n") || roverStartPosition.Contains("e") || roverStartPosition.Contains("s") || roverStartPosition.Contains("w")))
+                    {
+                        Console.WriteLine("Invalid input. Please use format \"x y rotation\" with rotation as N, E, S, or W.");
+                        continueCheck = false;
+                        newString = "";
+                    }
+                    else
+                    {
+                        roverX.Add(Int32.Parse(splited[0]));
+                        roverY.Add(Int32.Parse(splited[1]));
+
+                        switch (splited[2])
+                        {
+                            case "n":
+                                roverRotation.Add(Rotation.N);
+                                break;
+                            case "e":
+                                roverRotation.Add(Rotation.E);
+                                break;
+                            case "s":
+                                roverRotation.Add(Rotation.S);
+                                break;
+                            case "w":
+                                roverRotation.Add(Rotation.W);
+                                break;
+                        }
+                        continueCheck = true;
+                    }
+                }
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("An exception was thrown at Rover start position and rotation: " + err.StackTrace);
+                throw;
+            }
+            try
+            {
+                bool continueCheck = false;
+                while (!continueCheck)
+                {
+                    Console.Write("Rover movement: ");
+                    string roverInstructions = Console.ReadLine().Trim();
+                    string[] splited = roverInstructions.Split(" ");
+                    if (splited.Length != 1)
+                    {
+                        Console.WriteLine("Invalid input.");
+                        continueCheck = false;
+                    }
+                    else
+                    {
+                        movementInstructions.Add(splited[0]);
+                        continueCheck = true;
+                    }
+                }
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("An exception was thrown at Rover movement: " + err.StackTrace);
+                throw;
+            }
+            //Finished creating first rover with starting location and rotation and movement instructions.
+            Console.WriteLine("END");
+            Console.Write("Rover starting position (x y r): ");
+            string nextInput = Console.ReadLine().Trim().ToLower();
+            if (!string.IsNullOrWhiteSpace(nextInput))
+            {
+                newRover(nextInput);
+            }
+            return true;
+        }
+            
+    }
+}
+
